a3dee8a0fcbb56875acb6beb9b92f79559914b44
Mohammad Emon <mohammademon551@gmail.com>
2021-02-03 03:44:35 +0600

    null vs undefine

---
+++ null-vs-undefine.js
@@ -0,0 +1,23 @@
+
+let name;
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+const result = add(5, 8)
+
+const aboutMe = {name: 'Mohammad Emon', age: 20, address: 'Mohammadpur'}
+console.log(aboutMe.college);
+
+let fun = undefined;
+console.log(fun);
+
+const array = [15, 16, 17];
+console.log(array[5]);
+
+null
