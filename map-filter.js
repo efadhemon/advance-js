2be2aba0698c5ff4377fe9980eb28eecea285d85
Mohammad Emon <mohammademon551@gmail.com>
2021-02-03 04:47:50 +0600

    map filter js

---
+++ map-filter.js
@@ -0,0 +1,26 @@
+
+const numbers = [3, 4, 5, 6, 7, 8, 9];
+// const outPut = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     outPut.push(result);
+// }
+
+// function square(element){
+//     return element * element;
+// }
+
+// const result = numbers.map(function(element){
+//     return element * element;
+// });
+
+// const square = element=> element * element;
+// const result = numbers.map(square);
+
+const bigger = numbers.filter(x=> x > 5)
+
+const isThere = numbers.find(x=> x > 5)
+
+console.log(isThere);
