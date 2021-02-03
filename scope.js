9856fe4ecb9846635d987076688d7ee00f8be8a4
Mohammad Emon <mohammademon551@gmail.com>
2021-02-03 17:35:59 +0600

    scope js

---
+++ scope.js
@@ -0,0 +1,12 @@
+
+const bonus = 20;
+
+function sum(num1, num2) {
+    const result = num1 + num2 + bonus;
+    return result;
+}
+
+console.log(bonus);
+
+const output = sum(7, 5);
+console.log(output);
